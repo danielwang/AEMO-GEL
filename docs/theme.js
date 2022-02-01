d45fd9aace393f2db9fda078121e67546b1e6c25
danielwang <danielwang130@gmail.com>
2022-02-01 14:57:40 +1100

    feat: add wave svg

--- docs/theme.js
+++ docs/theme.js
@@ -2,13 +2,13 @@ module.exports = {
   maxWidth: "100%",
   sidebarWidth: 240,
   fontFamily: {
-    base: ["'Fira Sans'", "Helvetica", "Arial", "sans-serif"],
+    base: ["'Avenir LT Std'", "Helvetica", "Arial", "sans-serif"],
     monospace: ["Consolas", "'Liberation Mono'", "Menlo", "monospace"],
   },
   color: {
-    sidebarBackground: "#02172d",
+    sidebarBackground: "#360F3C",
     border: "rgba(255, 255, 255, 0.1)",
-    link: "#258aef",
-    linkHover: "#1070d1",
+    link: "#702F73",
+    linkHover: "#34B9B3",
   },
 }
